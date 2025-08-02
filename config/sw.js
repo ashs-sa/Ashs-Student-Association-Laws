9134e96144ed3da9c02cae8cd45bdbccb2e72e7e
ashssa <ashssa@users.noreply.github.com>
2025-08-02 18:51:41 +0000

    deploy: 8951168af1fca664ce067e70e8bbfd40d9317a0e

--- config/sw.js
+++ config/sw.js
@@ -1,5 +1,5 @@
 // 將 CACHE_NAME 設為一個佔位符，讓自動化腳本替換
-const CACHE_NAME = 'my-pwa-cache-v-eee718a'; // 注意這裡的 eee718a
+const CACHE_NAME = 'my-pwa-cache-v-8951168'; // 注意這裡的 8951168
 const urlsToCache = [
 '../Ashs-Student-Association-Laws/',
 '../Ashs-Student-Association-Laws/index.html',
